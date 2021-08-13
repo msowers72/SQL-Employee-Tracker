6582740e948a5f09c5bc9cececbd4192c0483aab
Mike Sowers <msowers72@yahoo.com>
2021-08-13 17:41:41 -0400

    added schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,18 +1,28 @@
-DROP DATABASE IF EXISTS movies_db;
-CREATE DATABASE movies_db;
+DROP DATABASE IF EXISTS tracker_db;
+CREATE DATABASE tracker_db;
 
-USE movies_db;
+USE tracker_db;
 
-CREATE TABLE movies (
+CREATE TABLE dpartment (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  movie_name VARCHAR(100) NOT NULL
+  name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE reviews (
+CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    movie_id INT,
-    review TEXT NOT NULL,
-    FOREIGN KEY (movie_id)
-    REFERENCES movies(id)
-    ON DELETE SET NULL
+    title INT,
+    salary DECIMAL,
+    department_id TEXT NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    REFERENCES role(id) REFERENCES employee(id) 
 );
