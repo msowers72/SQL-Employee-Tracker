d77b776dc85a21a89e59d04709b436cea63ffbf4
Mike Sowers <msowers72@yahoo.com>
2021-08-14 00:47:34 -0400

    added functions to index.js

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const mysql = require('mysql2');
 const table = require('console.table');
 const { allowedNodeEnvironmentFlags } = require('process');
 
+const allStaff = []
+const theManagers = []
+
 //data base
 const db = mysql.createConnection(
   {
@@ -16,7 +19,7 @@ const db = mysql.createConnection(
 
 
 
-goPrompt();
+
 
 function goPrompt() {
   inquirer.prompt([
@@ -33,8 +36,8 @@ function goPrompt() {
         "Plus Department"
       ]
     }
-  ]).then(function (input) {
-    switch (input.choice) {
+  ]).then(function (data) {
+    switch (data.choice) {
       case "Check Departments":
         viewDepartments()
         break;
@@ -103,8 +106,8 @@ function plusEmployees() {
       name: "manager",
       type: "list",
       message: "Manager:",
-      choices: selectManger()
-    },
+      choices: selectManager()
+    }
   ])
     .then(function (data) {
       var roleId = selectRole().indexOf(data.role) + 1
@@ -113,23 +116,131 @@ function plusEmployees() {
       db.query('INSERT INTO employee SET ?',
         {
           first_name: data.first_name,
-          lastname: data.last_name,
+          last_name: data.last_name,
           manager_id: managerid,
           role_id: roleId
-        }, funciton(err)
-      )
-      console.table(data)
-      goPrompt()
+        }, function (err) {
+          console.table(data)
+          goPrompt()
+        })
     })
 }
 // function for allstaff
- var allStaff = []
+
 function selectRole() {
   db.query('SELECT title FROM role', function (err, res) {
     for (var i = 0; i < res.length; i++) {
       allStaff.push(res[i].title)
     }
   })
-    return allStaff;
+  return allStaff;
 }
 
+
+function selectManager() {
+  db.query('SELECT first_name FROM employee WHERE manager_id IS NULL', function (err, res) {
+    for (var i = 0; i < res.length; i++) {
+      theManagers.push(res[i].first_name)
+    }
+  })
+  return theManagers;
+}
+
+function plusDepartment() {
+  {
+    inquirer.prompt([
+      {
+        name: "department",
+        type: "input",
+        message: "Enter the new department name: "
+
+      }
+    ])
+      .then(function (data) {
+
+        db.query('INSERT INTO department SET ?',
+          {
+            name: data.department,
+          }, function (err) {
+          console.table(data)
+          goPrompt()
+        })
+      })
+  }
+}
+
+function plusRole()
+{
+  db.query('SELECT role.title AS selectedTitle, role.salary AS selectedSalary FROM role',function (err,res)
+  {
+    inquirer.prompt([
+      {
+        name: "selectedTitle",
+        type: "input",
+        message: "Enter the role you'd like to add: "
+      },
+      {
+        name: "selectedSalary",
+        type: "input",
+        message: "What is the salary of this role?"
+      }
+    ]).then(function (res)
+    {
+      db.query("INSERT INTO role SET ?",
+        {
+          title: res.selectedTitle,
+          salary: res.selectedSalary,
+        },
+        function (err)
+        {
+          console.table(res)
+          goPrompt()
+        }
+      )
+    })
+  })
+}
+
+function plusEmployees()
+{
+  db.query('SELECT * FROM employee JOIN role ON employee.role_id = role.id;',function (err,res)
+  {
+    inquirer.prompt([
+      {
+        type: "list",
+        name: "updateId",
+        message: "Select an employee to update: ",
+        choices: function ()
+        {
+          var allEmployees = []
+          for (var i = 0; i < res.length; i++) {
+            allEmployees.push(res[i].last_name)
+          }
+          return allEmployees
+        },
+      },
+      {
+        name: "role",
+        type: 'list',
+        message: "What is the Employee's new role?",
+        choices: selectRole()
+      },
+    ]).then(function (data)
+    {
+      var roleId = selectRole().indexOf(data.role) + 1
+      db.query('UPDATE employee SET WHERE ?',
+        {
+          id: data.updateId,
+          role_id: data.roleId
+        },function (err) 
+      {
+        console.table(data)
+        goPrompt()
+      })
+    })
+  });
+}
+
+
+
+goPrompt();
